e7f57c6cf3ec804393794d98ca1ec995f7fa7ad8
the Nomai <futao3209@gmail.com>
2026-05-27 00:31:25 +0800

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,24 +1,19 @@
 import { defineConfig } from 'vitepress'
-import mathjax3 from 'markdown-it-mathjax3'
 
 export default defineConfig({
-  title: 'CYBER//BLOG',
-  description: 'A cyberpunk personal blog',
-  base: '/', // 用户页面仓库，根路径就是 /
-  outDir: '.vitepress/dist', // 构建输出目录
-  markdown: {
-    config: (md) => {
-      md.use(mathjax3, {
-        tex: {
-          inlineMath: [['$', '$'], ['\\(', '\\)']],
-          displayMath: [['$$', '$$'], ['\\[', '\\]']]
-        }
-      })
-    }
-  },
+  title: "CYBER BLOG",
+  description: "Cyberpunk Personal Blog",
+
   themeConfig: {
+    siteTitle: "NEON//BLOG",
+
     nav: [
-      { text: 'Home', link: '/' }
+      { text: "Home", link: "/" },
+      { text: "Posts", link: "/posts/" }
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/" }
     ]
   }
 })
